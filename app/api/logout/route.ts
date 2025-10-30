59e56d5d40bd6f96e84b78e44f317de400a28a30
harshillencircle <harshill.encircle@gmail.com>
2025-10-30 17:51:11 +0530

    Update route.ts

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,7 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const response = NextResponse.redirect(new URL("/login", request.url));
+export async function POST(request: NextRequest) {
+  const redirecturl = new URL('/login', request.url);
+  
+  const response = NextResponse.redirect(redirecturl);
   response.cookies.set("user", "", { expires: new Date(0), path: "/" });
   return response;
 }
